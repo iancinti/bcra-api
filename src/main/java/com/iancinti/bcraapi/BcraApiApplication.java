d951dbaa258adf08c881a98bc040fa7ff36e344b
iancinti <ian.cinti@gmail.com>
2024-05-01 23:30:32 -0300

    Initial commit

---
+++ src/main/java/com/iancinti/bcraapi/BcraApiApplication.java
@@ -0,0 +1,13 @@
+package com.iancinti.bcraapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcraApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iancinti/bcraapi/BcraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iancinti.bcraapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
